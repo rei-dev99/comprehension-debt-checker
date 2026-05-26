d1604f0fdb1aac58b845d21e511c21443d3331a7
rei <idareiya123@gmail.com>
2026-05-27 06:13:51 +0900

    fix: response json @choices

--- back/app/controllers/api/v1/choices_controller.rb
+++ back/app/controllers/api/v1/choices_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ChoicesController < ApplicationController
   def index
     @choices = Choice.all
-    render status: :ok, json: @choices
+    render status: :ok,
+    json: @choices.as_json(
+      only: [ :id, :content, :score, :question_id ]
+    )
   end
 end
