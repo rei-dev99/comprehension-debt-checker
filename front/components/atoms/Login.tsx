5bf70003ab94d54f6bfb4bb231886a8a2d8bf751
rei <idareiya123@gmail.com>
2026-06-23 07:35:51 +0900

    add: google icon

--- front/components/atoms/Login.tsx
+++ front/components/atoms/Login.tsx
@@ -1,14 +1,25 @@
 "use client";
 import { handleGoogleLogin } from "@/actions/handleGoogleLogin";
+import Image from "next/image";
 
 export default function Login() {
 	return (
 		<form action={handleGoogleLogin}>
 			<button
 				type="submit"
-				className="cursor-pointer border px-3 py-2 rounded-md hover:opacity-70 transition"
+				className="
+					flex items-center gap-3
+					border border-gray-300
+					rounded-md
+					px-4 py-2
+					bg-white
+					hover:bg-gray-50
+					transition
+					cursor-pointer
+				"
 			>
-				Google でログイン
+				<Image src="/google.svg" alt="Googleアイコン" width={20} height={20} />
+				<span className="text-sm font-medium text-gray-700">Googleで続行</span>
 			</button>
 		</form>
 	);
