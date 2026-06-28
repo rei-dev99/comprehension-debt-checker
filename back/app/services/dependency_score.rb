7bc79fd9d0c5cba7767a8775f06d4b5f6474df38
rei <idareiya123@gmail.com>
2026-06-28 16:27:34 +0900

    fix: AI依存度算出を実装

--- back/app/services/dependency_score.rb
+++ back/app/services/dependency_score.rb
@@ -1,72 +1,56 @@
 class DependencyScore
+  # 各質問ごとの重み。
+  #
+  # score
+  # 3 = 良い回答（依存度0）
+  # 2 = 中間回答
+  # 1 = 依存度が高い回答
+  #
+  # 数値が大きいほどAI依存度が高くなる。
+  # 合計値は100を上限とし将来的に診断内容に合わせて調整可能
+  BASE = {
+    # Q1 エラーが出た時、まず何をしますか？
+    1 => { 1 => 20, 2 => 10, 3 => 0 },
+
+    # Q2 AIの回答は、どのように扱いますか？
+    2 => { 1 => 30, 2 => 10, 3 => 0 },
+
+    # Q3 AIを使う前に、自分で確認することはありますか？
+    3 => { 1 => 20, 2 => 5, 3 => 0 },
+
+    # Q4 書いたコードを見て、意図を説明できますか？
+    4 => { 1 => 10, 2 => 10,  3 => 0 },
+
+    # Q5 AIを自分にとってどう使うものだと思いますか？
+    5 => { 1 => 30, 2 => 15, 3 => 0 }
+  }.freeze
+
   def initialize(answers)
     @answers = answers
   end
 
   def call
-    scores = set_scores
-    dependency_score = calculate_dependency(scores)
-    [ scores, dependency_score ]
+    calculate_dependency
   end
 
   private
 
-  def set_scores
-    scores = {
-      ai: 0,
-      algorithm: 0,
-      db: 0,
-      web: 0
-    }
+  def calculate_dependency
+    dependency = 0
 
-    @answers.each do |question_id, choice_id|
+    @answers.each do |_question_id, choice_id|
       choice = Choice.find(choice_id)
       question = choice.question
       category = question.category
 
-      case category.name
-      when "AI活用習慣"
-        scores[:ai] += choice.score
-      when "アルゴリズム基礎"
-        scores[:algorithm] += choice.score
-      when "データベース"
-        scores[:db] += choice.score
-      when "Web基礎"
-        scores[:web] += choice.score
-      end
+      weight = BASE[question.id]
+      next unless weight
+
+      dependency += weight[choice.score]
     end
 
-    scores
-  end
+    dependency = [ dependency, 100 ].min
 
-  def calculate_dependency(scores)
-    base = scores[:algorithm] + scores[:db] + scores[:web]
-    ai = scores[:ai]
-    adjusted_base = adjust_base(base)
-
-    dependency = 0
-
-    if ai == 0
-      dependency = 100
-    else
-      ideal_base = ai * 3
-      if adjusted_base >= ideal_base
-        dependency = 0
-      else
-        dependency = ((ideal_base - adjusted_base).to_f / ideal_base) * 100
-      end
-    end
-
-    dependency.floor
-  end
-
-  def adjust_base(score)
-    if score < 20
-        score = score * 0.8
-    elsif score < 30
-        score = score * 0.9
-    else
-        score
-    end
+    dependency
   end
 end
