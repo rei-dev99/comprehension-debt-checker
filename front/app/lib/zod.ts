ba6b21c96b5d98629b8c6fdfe48a788cc2376864
rei <idareiya123@gmail.com>
2026-07-02 21:54:32 +0900

    fix: validation & message

--- front/app/lib/zod.ts
+++ front/app/lib/zod.ts
@@ -2,12 +2,16 @@ import { z } from "zod";
 
 export const numberPattern = /(?=.*\d)/;
 
-export const signInSchema = z.object({
-	email: z.string().email("正しいメールアドレスを入力してください"),
+export const credentialSchema = z.object({
+	email: z
+	    .string()
+		.min(1, "メールアドレスを入力してください")
+		.email("正しいメールアドレスを入力してください"),
 	password: z
 		.string()
+		.min(1, "パスワードを入力してください")
 		.min(8, "8文字以上で入力してください")
 		.regex(numberPattern, "数字を含めてください"),
 });
 
-export type CredentialType = z.infer<typeof signInSchema>;
+export type CredentialType = z.infer<typeof credentialSchema>;
