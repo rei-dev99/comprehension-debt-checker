ba680ca18f068f179e8501ba6fc580a93eab109a
rei <idareiya123@gmail.com>
2026-07-04 10:52:23 +0900

    fix: biome lint

--- front/components/forms/SignupForm.tsx
+++ front/components/forms/SignupForm.tsx
@@ -1,107 +1,104 @@
-'use client';
+"use client";
 
 import { useState } from "react";
-import { credentialSchema } from "@/app/lib/zod";
-import handleSignup from "@/actions/handleSignup";
 import z from "zod";
+import handleSignup from "@/actions/handleSignup";
+import { credentialSchema } from "@/app/lib/zod";
 
 export function SignupForm() {
 	const [errors, setErrors] = useState({
-        email: "",
-        password: "",
-    });
+		email: "",
+		password: "",
+	});
 
-    const handleSubmit = async (formData: FormData) =>  {
+	const handleSubmit = async (formData: FormData) => {
+		setErrors({ email: "", password: "" });
 
-        setErrors({email: "", password: ""});
+		const email = String(formData.get("email") ?? "");
+		const password = String(formData.get("password") ?? "");
 
-        const email = String(formData.get("email") ?? "");
-        const password = String(formData.get("password") ?? "");
+		const validation = credentialSchema.safeParse({ email, password });
 
-        const validation = credentialSchema.safeParse({ email, password });
+		if (!validation.success) {
+			const errors = z.flattenError(validation.error).fieldErrors;
 
-        if (!validation.success) {
-            const errors = z.flattenError(validation.error).fieldErrors;
+			setErrors({
+				email: errors.email?.[0] ?? "",
+				password: errors.password?.[0] ?? "",
+			});
 
-            setErrors({
-                email: errors.email?.[0] ?? "",
-                password: errors.password?.[0] ?? "",
-            });
+			return;
+		}
 
-            return;
-        }
+		const result = await handleSignup({ email, password });
 
-        const result = await handleSignup({ email, password });
+		if (!result?.success) {
+			setErrors((prev) => ({
+				...prev,
+				[result?.error.field]: result?.error.message,
+			}));
+		}
+	};
 
-        if (!result?.success) {
-            setErrors(prev => ({
-                ...prev,
-                [result?.error.field]: result?.error.message,
-            }));
-        }
-    }
+	return (
+		<form className="border-b border-dashed pb-6" action={handleSubmit}>
+			<div className="mb-4">
+				<label className="block text-gray-700 text-sm font-bold mb-2">
+					メールアドレス
+				</label>
 
-    return (
-        <form className="border-b border-dashed pb-6" action={handleSubmit}>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">
-                    メールアドレス
-                </label>
-
-                <input
-                    className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+				<input
+					className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
                         ${
-                            errors.email
-                                ? "border-red-500 focus:ring-2 focus:ring-red-300"
-                                : "border-gray-300"
-                        }`
-                    }
-                    id="email"
-                    type="email"
-                    placeholder="メールアドレス"
-                    name="email"
-                />
+													errors.email
+														? "border-red-500 focus:ring-2 focus:ring-red-300"
+														: "border-gray-300"
+												}`}
+					id="email"
+					type="email"
+					placeholder="メールアドレス"
+					name="email"
+				/>
 
-                {errors.email && (
-                    <p className="mt-1 flex items-center gap-1 text-sm text-red-600">
-                        <span>⚠</span>
-                        <span>{errors.email}</span>
-                    </p>
-                )}
-            </div>
-            <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2">
-                    パスワード
-                </label>
-                <input
-                    className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+				{errors.email && (
+					<p className="mt-1 flex items-center gap-1 text-sm text-red-600">
+						<span>⚠</span>
+						<span>{errors.email}</span>
+					</p>
+				)}
+			</div>
+			<div className="mb-6">
+				<label className="block text-gray-700 text-sm font-bold mb-2">
+					パスワード
+				</label>
+				<input
+					className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
                         ${
-                            errors.password
-                                ? "border-red-500 focus:ring-2 focus:ring-red-300"
-                                : "border-gray-300"
-                        }`
-                    }
-                    id="password"
-                    type="password"
-                    placeholder="パスワード"
-                    name="password"
-                />
+													errors.password
+														? "border-red-500 focus:ring-2 focus:ring-red-300"
+														: "border-gray-300"
+												}`}
+					id="password"
+					type="password"
+					placeholder="パスワード"
+					name="password"
+				/>
 
-                {errors.password && (
-                    <p className="mt-1 flex items-center gap-1 text-sm text-red-600">
-                        <span>⚠</span>
-                        <span>{errors.password}</span>
-                    </p>
-                )}
-            </div>
-            <div className="flex justify-center">
-                <button
-                    className="cursor-pointer bg-blue-500 hover:bg-blue-700 transition text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit"
-                >
-                    登録する
-                </button>
-            </div>
-        </form>
-    )
+				{errors.password && (
+					<p className="mt-1 flex items-center gap-1 text-sm text-red-600">
+						<span>⚠</span>
+						<span>{errors.password}</span>
+					</p>
+				)}
+			</div>
+			<div className="flex justify-center">
+				<button
+					className="cursor-pointer bg-blue-500 hover:bg-blue-700 transition text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+					type="submit"
+				>
+					登録する
+				</button>
+			</div>
+		</form>
+	);
 }
